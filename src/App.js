33773a4cf03b8dfe75c2633d8fb34294f23b0762
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-05-09 10:29:16 -0400

    Track state, uh, state, in App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import us from './data/us.json';
 import districts from './data/us-congress-113.json';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      activeState: null,
+    };
+
+    this.updateActiveState = this.updateActiveState.bind(this);
+  }
+
+  updateActiveState(id) {
+    id
+      ? this.setState({ activeState: id })
+      : this.setState({ activeState: null });
+  }
+
   render() {
     return (
       <div className="App">
