1831ddc7e1896c85d133eabfae926749b879548b
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-05-10 13:19:39 -0400

    Add props for hover container position

--- src/components/HoverContainer.js
+++ src/components/HoverContainer.js
@@ -3,11 +3,17 @@ import styled from 'styled-components';
 
 const StyledHoverContainer = styled.div`
   background-color: #fff;
+  display: ${props => (props.active ? 'block' : 'none')};
+  position: fixed;
+  top: ${props => props.y}px;
   width: 300px;
+  z-index: 100;
 `;
 
 const HoverContainer = props => {
-  return <StyledHoverContainer>{props.contents}</StyledHoverContainer>;
+  return (
+    <StyledHoverContainer {...props}>{props.children}</StyledHoverContainer>
+  );
 };
 
 export default HoverContainer;
