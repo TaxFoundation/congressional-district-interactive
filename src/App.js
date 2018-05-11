ba8622fcb1d3bc64a8bc59f55f84414005a1b2c4
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-05-11 13:15:32 -0400

    Prep for data navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation';
 import USMap from './components/USMap';
 import StateMap from './components/StateMap';
 import us from './data/us.json';
 import districts from './data/us-congress-113.json';
+import { csvParse } from 'd3-dsv';
+import data from './data/data';
 
 class App extends Component {
   constructor() {
@@ -10,18 +13,43 @@ class App extends Component {
 
     this.state = {
       activeState: null,
+      income: '30-75',
+      filingStatus: 1,
+      children: 2,
     };
 
     this.updateActiveState = this.updateActiveState.bind(this);
+    this.updateIncome = this.updateIncome.bind(this);
+    this.updateStatus = this.updateStatus.bind(this);
+    this.updateChildren = this.updateChildren.bind(this);
+
+    this.data = csvParse(data);
   }
 
   updateActiveState(id) {
     this.setState({ activeState: id });
   }
 
+  updateIncome(income) {
+    this.setState({ income });
+  }
+
+  updateStatus(filingStatus) {
+    this.setState({ filingStatus });
+  }
+
+  updateChildren(children) {
+    this.setState({ children });
+  }
+
   render() {
     return (
       <div className="App">
+        <Navigation
+          updateIncome={this.updateIncome}
+          updateStatus={this.updateStatus}
+          updateChildren={this.updateChildren}
+        />
         {this.state.activeState ? (
           <StateMap
             activeState={this.state.activeState}
@@ -29,6 +57,7 @@ class App extends Component {
           />
         ) : (
           <USMap
+            data={this.data}
             us={us}
             districts={districts}
             updateActiveState={this.updateActiveState}
---
+++ src/components/Navigation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const FamilySelection = props => {
+  return <div />;
+};
+
+const StateSelection = props => {
+  return <div />;
+};
+
+const DistrictSelection = props => {
+  return <div />;
+};
+
+const Navigation = props => {
+  return (
+    <div>
+      <FamilySelection />
+      <StateSelection />
+      <DistrictSelection />
+    </div>
+  );
+};
+
+export default Navigation;
