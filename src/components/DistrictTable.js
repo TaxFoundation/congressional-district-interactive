1fcfaadfd934956d6bf8a861aee973165f86bcbb
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-06-07 11:27:42 -0400

    Fix table bug with initial activeDistrict issues

--- src/components/DistrictTable.js
+++ src/components/DistrictTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Button from './Button';
 import Select from './Select';
@@ -43,70 +43,84 @@ const ValueCell = styled.td`
   text-align: right;
 `;
 
-const DistrictTable = props => {
-  let theData = { i: '', s: '', t: '' };
-  let bucketText = '';
-  if (props.data && props.activeBucket >= 0 && props.activeDistrict >= 0) {
-    theData = props.data[props.activeDistrict][props.activeBucket];
-    bucketText = BUCKETS.find(b => +b.id === +props.activeBucket).value;
+class DistrictTable extends Component {
+  constructor(props) {
+    super(props);
   }
 
-  return (
-    <StyledDistrictTable>
-      <div>
-        {Object.keys(props.data).length > 1 ? (
-          <Select
-            name="district"
-            id="district"
-            value={props.activeDistrict}
-            onChange={e => props.updateActiveDistrict(e.target.value)}
-          >
-            {Object.keys(props.data).map(d => (
-              <option key={`district-opt-${d}`} value={+d}>
-                {`District ${d}`}
-              </option>
-            ))}
-          </Select>
-        ) : props.activeState === 11 ? (
-          <h3 style={{ textAlign: 'center' }}>District of Columbia</h3>
-        ) : (
-          <h3 style={{ textAlign: 'center' }}>At-Large District</h3>
-        )}
-        {theData ? (
-          <Table>
-            <tbody>
-              <tr>
-                <td>Avg. Tax Cut</td>
-                <ValueCell color={theData.t >= 0 ? '#00aa22' : '#ef4438'}>
-                  {formatter(theData.t, '$')}
-                </ValueCell>
-              </tr>
-              <tr>
-                <td>Avg. Tax Cut as % of Income</td>
-                <ValueCell color={theData.t >= 0 ? '#00aa22' : '#ef4438'}>
-                  {formatter(theData.t / theData.i, '%')}
-                </ValueCell>
-              </tr>
-              <tr>
-                <td>{`Avg. Income from ${bucketText}`}</td>
-                <ValueCell>{formatter(theData.i, '$')}</ValueCell>
-              </tr>
-              <tr>
-                <td>Avg. State Taxes Paid</td>
-                <ValueCell>{formatter(theData.s, '$')}</ValueCell>
-              </tr>
-            </tbody>
-          </Table>
-        ) : null}
-      </div>
-      <Button
-        style={{ alignSelf: 'end' }}
-        onClick={e => props.updateActiveState(0)}
-      >
-        Go Back to US Map
-      </Button>
-    </StyledDistrictTable>
-  );
-};
+  render() {
+    let theData = { i: '', s: '', t: '' };
+    let bucketText = '';
+    let theDistrict =
+      Object.keys(this.props.data).length > 1 && this.props.activeDistrict === 0
+        ? 1
+        : this.props.activeDistrict;
+    if (
+      this.props.data &&
+      this.props.activeBucket >= 0 &&
+      this.props.activeDistrict >= 0
+    ) {
+      theData = this.props.data[theDistrict][this.props.activeBucket];
+      bucketText = BUCKETS.find(b => +b.id === +this.props.activeBucket).value;
+    }
+
+    return (
+      <StyledDistrictTable>
+        <div>
+          {Object.keys(this.props.data).length > 1 ? (
+            <Select
+              name="district"
+              id="district"
+              value={this.props.activeDistrict}
+              onChange={e => this.props.updateActiveDistrict(e.target.value)}
+            >
+              {Object.keys(this.props.data).map(d => (
+                <option key={`district-opt-${d}`} value={+d}>
+                  {`District ${d}`}
+                </option>
+              ))}
+            </Select>
+          ) : this.props.activeState === 11 ? (
+            <h3 style={{ textAlign: 'center' }}>District of Columbia</h3>
+          ) : (
+            <h3 style={{ textAlign: 'center' }}>At-Large District</h3>
+          )}
+          {theData ? (
+            <Table>
+              <tbody>
+                <tr>
+                  <td>Avg. Tax Cut</td>
+                  <ValueCell color={theData.t >= 0 ? '#00aa22' : '#ef4438'}>
+                    {formatter(theData.t, '$')}
+                  </ValueCell>
+                </tr>
+                <tr>
+                  <td>Avg. Tax Cut as % of Income</td>
+                  <ValueCell color={theData.t >= 0 ? '#00aa22' : '#ef4438'}>
+                    {formatter(theData.t / theData.i, '%')}
+                  </ValueCell>
+                </tr>
+                <tr>
+                  <td>{`Avg. Income from ${bucketText}`}</td>
+                  <ValueCell>{formatter(theData.i, '$')}</ValueCell>
+                </tr>
+                <tr>
+                  <td>Avg. State Taxes Paid</td>
+                  <ValueCell>{formatter(theData.s, '$')}</ValueCell>
+                </tr>
+              </tbody>
+            </Table>
+          ) : null}
+        </div>
+        <Button
+          style={{ alignSelf: 'end' }}
+          onClick={e => this.props.updateActiveState(0)}
+        >
+          Go Back to US Map
+        </Button>
+      </StyledDistrictTable>
+    );
+  }
+}
 
 export default DistrictTable;
